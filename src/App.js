e7c1f45a753abc1a4c7b9f66cacaa3d3812002d0
Tessa Gonzales <tessaleego@gmail.com>
2018-04-05 11:59:42 -0700

    api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import Input from './components/Input.js';
-import Definition from './components/Definition.js';
-import DefinitionList from './components/DefinitionList.js';
+import Input from './components/Input';
+import DefinitionList from './components/DefinitionList';
+import axios from 'axios'
+
 import './App.css';
 
 class App extends Component {
 
-  handleSearch = (searchTerm)=>{
-    console.log(searchTerm);
+  state = {
+    urbanDic: {}
   }
+
+componentDidMount(){
+  axios.get(`http://api.urbandictionary.com/v0/define?term=wat`)
+  .then(urbanDic => this.setState({ urbanDic :urbanDic.data }))
+}
+
+searchTerm(term){
+  axios.get(`http://api.urbandictionary.com/v0/define?term=${term}`)
+  .then(urbanDic => this.setState({ urbanDic :urbanDic.data }))
+}
+
   render() {
+    console.log(this.state.urbanDic)
     return (
       <div className="">
-        <Input search={(searchTerm)=>{this.handleSearch(searchTerm)}}/>
+        <Input />
       </div>
     );
   }
